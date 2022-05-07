64c5a3b7f5b890712c33dd3724088a6b1c9a2f94
danielmserna <danielm.serna@hotmail.com>
2022-05-07 15:35:17 -0500

    Add spotify jar

--- app/src/main/java/com/jelp/spoticar/SpotiCarActivity.java
+++ app/src/main/java/com/jelp/spoticar/SpotiCarActivity.java
@@ -25,4 +25,20 @@ public class SpotiCarActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
 }
